3a98cf9e889ec0ea9632883e1fcebdd3f9cbe76c
David Orchard <github@sigpanic.com>
2025-03-19 15:38:43 -0700

    Update immich

--- hosts/drywell/services/immich.nix
+++ hosts/drywell/services/immich.nix
@@ -42,7 +42,7 @@ in {
 
     services = {
       immich = {
-        image = "ghcr.io/immich-app/immich-server:v1.124.2";
+        image = "ghcr.io/immich-app/immich-server:v1.129.0";
 
         environment = {
           TZ = "America/Los_Angeles";
