439ff721514e58ef9516665c61b68418dbea1d89
David Orchard <github@sigpanic.com>
2025-04-10 15:11:50 -0700

    DDNS for IPv6

--- hosts/cuttlefish/services/ddns.nix
+++ hosts/cuttlefish/services/ddns.nix
@@ -7,44 +7,53 @@
 }:
 with builtins;
 with lib; let
-  configFile = pkgs.writeText "godns.json" (toJSON {
-    provider = "Cloudflare";
-    login_token = "API Token";
-    ip_type = "IPv4";
-    proxied = true;
-    resolver = "8.8.8.8";
-    interval = 300;
-    debug_info = true;
+  serviceSettings = rec {
+    godns-IPv4 = {
+      provider = "Cloudflare";
+      login_token = "API Token";
+      ip_type = "IPv4";
+      proxied = true;
+      resolver = "1.1.1.1";
+      interval = 300;
+      debug_info = true;
 
-    ip_urls = [
-      "https://ip4.seeip.org"
-      "https://api.ipify.org"
-      "https://myip.biturl.top"
-      "https://ipecho.net/plain"
-      "https://api-ipv4.ip.sb/ip"
-    ];
+      ip_urls = [
+        "https://ip4.seeip.org"
+        "https://api.ipify.org"
+        "https://myip.biturl.top"
+        "https://ipecho.net/plain"
+        "https://api-ipv4.ip.sb/ip"
+      ];
 
-    domains = [
-      {
-        domain_name = "sigpanic.com";
-        sub_domains = ["@"];
-      }
-    ];
-  });
+      domains = [
+        {
+          domain_name = "sigpanic.com";
+          sub_domains = ["@"];
+        }
+      ];
+    };
 
-  RuntimeDirectory = "godns";
-in {
-  config = {
-    systemd.services.godns = {
+    godns-IPv6 = godns-IPv4 // {
+      ip_urls = [];
+      ip_type = "IPv6";
+      ip_interface = "cuttlefish";
+    };
+  };
+
+  createService = name: settings:
+    let
+      configFile = pkgs.writeText "godns-${name}.json" (toJSON settings);
+    in {
       description = "Dynamic DNS Client";
       wantedBy = ["multi-user.target"];
       after = ["network.target"];
       restartTriggers = [configFile];
 
-      serviceConfig = {
+      serviceConfig = rec {
         DynamicUser = true;
-        inherit RuntimeDirectory;
-        ExecStartPre = "!${pkgs.writeShellScript "godns-prestart" ''
+        RuntimeDirectory = name;
+
+        ExecStartPre = "!${pkgs.writeShellScript "${name}-prestart" ''
           install --mode=600 --owner=$USER "${configFile}" "/run/${RuntimeDirectory}/godns.json"
           "${pkgs.replace-secret}/bin/replace-secret" "API Token" "${config.sops.secrets.cloudflare-ddns.path}" "/run/${RuntimeDirectory}/godns.json"
         ''}";
@@ -52,5 +61,6 @@ in {
         ExecStart = "${pkgs.godns}/bin/godns -c /run/${RuntimeDirectory}/godns.json";
       };
     };
-  };
+in {
+  config.systemd.services = mapAttrs createService serviceSettings;
 }
