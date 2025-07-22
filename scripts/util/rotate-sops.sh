ed04e36493b25e2aafd1ccb24dabc4c5c54369df
David Orchard <david@interchange.com>
2025-07-22 11:22:37 -0700

    Update sops key

--- scripts/util/rotate-sops.sh
+++ scripts/util/rotate-sops.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for regex in $(yq  '.creation_rules | map(.path_regex) | .[]' .sops.yaml | sed 's#^\^#^./#'); do
-    find . -regex "$regex" -exec sops updatekeys -y '{}' ';'
+    find . -regextype posix-extended -regex "$regex" -exec sops updatekeys -y '{}' ';'
 done
