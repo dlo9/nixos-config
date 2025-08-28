50a4cde35f9367c35c624137760a46cd82828051
David Orchard <if_coding@fastmail.com>
2025-08-27 18:21:17 -0700

    Limit journald storage

--- system/nixos/default.nix
+++ system/nixos/default.nix
@@ -75,6 +75,12 @@ with lib; {
     ];
   };
 
+  services.journald.extraConfig = ''
+    SystemMaxUse=256M    # Maximum total journal size
+    RuntimeMaxUse=128M   # Maximum journal size in temporary storage
+    MaxFileSec=1month    # Maximum time to retain log files
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
