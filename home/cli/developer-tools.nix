2716b2d97c8bbdf79e5536bb533932e706ffb3a7
David Orchard <github@sigpanic.com>
2025-11-11 14:40:22 -0800

    Remove upstreamed packages

--- home/cli/developer-tools.nix
+++ home/cli/developer-tools.nix
@@ -33,17 +33,17 @@ with lib; {
           unstable.devenv
           gitu # Git TUI
           # dlo9.pocker # Docker TUI - too many python deps
-          dlo9.toolong
+          toolong
           tcping-go
 
           dlo9.havn # Port scanner
           dlo9.cidr
           dlo9.pvw # Port viewer, pvw -aon
-          dlo9.carl
+          carl
           dlo9.cy
-          dlo9.posting # Postman-like clint
-          # dlo9.otree # JSON tree viewer
-          # dlo9.rainfrog # Postgres TUI
+          posting # Postman-like clint
+          otree # JSON tree viewer
+          rainfrog # Postgres TUI
           dlo9.somo
 
           flashrom
