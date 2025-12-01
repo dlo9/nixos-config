3c066beb9e0e7384485cb8817fa3b2c3cc69f8df
David Orchard <github@sigpanic.com>
2025-11-30 21:28:44 -0800

    Remove flox

--- system/nix.nix
+++ system/nix.nix
@@ -26,7 +26,6 @@ with lib; {
         # See priority of each cache: curl https://cache.nixos.org/nix-cache-info
         "https://nix-community.cachix.org?priority=50"
         "https://cuda-maintainers.cachix.org?priority=60"
-        "https://cache.flox.dev"
 
         # https://github.com/nix-community/nix-on-droid/wiki/Remote-deploy-with-deploy%E2%80%90rs
         "https://nix-on-droid.cachix.org"
@@ -35,7 +34,6 @@ with lib; {
       trusted-substituters = [
         "https://nix-community.cachix.org"
         "https://cuda-maintainers.cachix.org"
-        "https://cache.flox.dev"
         "https://devenv.cachix.org"
         "https://nix-serve.sigpanic.com"
       ];
@@ -44,7 +42,6 @@ with lib; {
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-serve.sigpanic.com:fp2dLidIBUYvB1SgcAAfYIaxIvzffQzMJ5nd/jZ+hww="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-        "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
         "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
       ];
     };
