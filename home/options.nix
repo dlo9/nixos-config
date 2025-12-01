7f56c696dccc403406b96eecfa59d965f879813b
David Orchard <github@sigpanic.com>
2025-11-30 21:28:44 -0800

    Fix wallpapers

--- home/options.nix
+++ home/options.nix
@@ -50,34 +50,16 @@ with builtins; {
         #  sha256 = "0p6z31gh552rk4w99gbvr3hvwadfrv6h97k41qdbb9mxy7wc9brz";
         #};
 
-        mushroom = fetchurl {
-          name = "mushroom";
-          url = https://images.squarespace-cdn.com/content/v1/5de93a2db580764b4f6963f9/f21eae05-9d02-40a7-ac60-a728c961eba0/BRONZE%C2%A9Antonio+Coelho_Foggy+morning.jpg;
-          sha256 = "1fbwmcdq92263s0vsnsjj1cgy62kvzv8v7px7w8dgg7s3lp90lzy";
-        };
-
         mountain-milky-way = fetchurl {
           name = "mountain-milky-way";
           url = https://images.squarespace-cdn.com/content/v1/5de93a2db580764b4f6963f9/10757377-0072-4864-bcf9-17bc4df0d252/GOLD%C2%A9Jake+Mosher_The+Grand+Tetons.jpg;
-          sha256 = "19l11x94qyc7mqwkrr8l2llimqp7v38ikd66k3pvma0vb3773js3";
+          sha256 = "1sri7zmxcck1c11v5sy6dd3ypr7hbrmrqia3sd21rsb9yfallkmg";
         };
 
         mountain-reflection = fetchurl {
           name = "mountain-reflection";
           url = https://images.squarespace-cdn.com/content/v1/5de93a2db580764b4f6963f9/956426d5-8a84-493d-af76-fee19de5a29d/SILVER%C2%A9Beatrice+Wong_Parallel+universe.jpg;
-          sha256 = "0qy0i3wfd3ivzmnkcg398milyczsqggyryqhz6rpajvg5v1r580f";
-        };
-
-        lightning-cloud = fetchurl {
-          name = "lightning-cloud";
-          url = https://images.squarespace-cdn.com/content/v1/5de93a2db580764b4f6963f9/63ff2ce8-8b10-48a1-8825-250f8d7f6759/BRONZE%C2%A9Miki+Spitzer_Storm+clouds+over+a+farm.jpg;
-          sha256 = "1x7xjxib7ccnhx4a0zb477v1k3ajgq9miksj26gaa6lsjh879f80";
-        };
-
-        birds-with-red-background = fetchurl {
-          name = "birds-with-red-background";
-          url = https://images.squarespace-cdn.com/content/v1/5de93a2db580764b4f6963f9/f31fc21f-e6cd-407d-bc72-1a856ab56c75/BRONZE%C2%A9Silke+Hullmann_On+their+way+to+Mars.jpg;
-          sha256 = "141yp07q4gk0gmj8nd1d66zf8jn3vbgrbqbzzg77ryjkhxqj2vk9";
+          sha256 = "173spa2j1fbvgzag3lw3y3sl7zpags4mixrwx7fv0brmp483v8g7";
         };
 
         # Blocked by cloudflare
