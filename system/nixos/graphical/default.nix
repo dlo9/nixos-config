a3872bdf4d68209c1ab52b040ec00938e7ed4660
David Orchard <github@sigpanic.com>
2026-07-31 11:39:47 -0700

    Disable alsa

--- system/nixos/graphical/default.nix
+++ system/nixos/graphical/default.nix
@@ -22,8 +22,6 @@ with lib; {
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
       pulse.enable = true;
     };
 
